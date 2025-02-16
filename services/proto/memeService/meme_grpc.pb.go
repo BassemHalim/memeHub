034c0d3eb3c8372d9d6a3903d87231dc6c4435d9
BassemHalim <bassemamir459@gmail.com>
2025-02-15 23:18:28 -0800

    added proto function for timeline

--- services/proto/memeService/meme_grpc.pb.go
+++ services/proto/memeService/meme_grpc.pb.go
@@ -23,6 +23,7 @@ const (
 	MemeService_GetMeme_FullMethodName           = "/meme.MemeService/GetMeme"
 	MemeService_FilterMemesByTags_FullMethodName = "/meme.MemeService/FilterMemesByTags"
 	MemeService_SearchMemes_FullMethodName       = "/meme.MemeService/SearchMemes"
+	MemeService_GetTimelineMemes_FullMethodName  = "/meme.MemeService/GetTimelineMemes"
 )
 
 // MemeServiceClient is the client API for MemeService service.
@@ -33,6 +34,7 @@ type MemeServiceClient interface {
 	GetMeme(ctx context.Context, in *GetMemeRequest, opts ...grpc.CallOption) (*MemeResponse, error)
 	FilterMemesByTags(ctx context.Context, in *FilterMemesByTagsRequest, opts ...grpc.CallOption) (*MemesResponse, error)
 	SearchMemes(ctx context.Context, in *SearchMemesRequest, opts ...grpc.CallOption) (*MemesResponse, error)
+	GetTimelineMemes(ctx context.Context, in *GetTimelineRequest, opts ...grpc.CallOption) (*MemesResponse, error)
 }
 
 type memeServiceClient struct {
@@ -83,6 +85,16 @@ func (c *memeServiceClient) SearchMemes(ctx context.Context, in *SearchMemesRequ
 	return out, nil
 }
 
+func (c *memeServiceClient) GetTimelineMemes(ctx context.Context, in *GetTimelineRequest, opts ...grpc.CallOption) (*MemesResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(MemesResponse)
+	err := c.cc.Invoke(ctx, MemeService_GetTimelineMemes_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MemeServiceServer is the server API for MemeService service.
 // All implementations must embed UnimplementedMemeServiceServer
 // for forward compatibility.
@@ -91,6 +103,7 @@ type MemeServiceServer interface {
 	GetMeme(context.Context, *GetMemeRequest) (*MemeResponse, error)
 	FilterMemesByTags(context.Context, *FilterMemesByTagsRequest) (*MemesResponse, error)
 	SearchMemes(context.Context, *SearchMemesRequest) (*MemesResponse, error)
+	GetTimelineMemes(context.Context, *GetTimelineRequest) (*MemesResponse, error)
 	mustEmbedUnimplementedMemeServiceServer()
 }
 
@@ -112,6 +125,9 @@ func (UnimplementedMemeServiceServer) FilterMemesByTags(context.Context, *Filter
 }
 func (UnimplementedMemeServiceServer) SearchMemes(context.Context, *SearchMemesRequest) (*MemesResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SearchMemes not implemented")
+}
+func (UnimplementedMemeServiceServer) GetTimelineMemes(context.Context, *GetTimelineRequest) (*MemesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetTimelineMemes not implemented")
 }
 func (UnimplementedMemeServiceServer) mustEmbedUnimplementedMemeServiceServer() {}
 func (UnimplementedMemeServiceServer) testEmbeddedByValue()                     {}
@@ -206,6 +222,24 @@ func _MemeService_SearchMemes_Handler(srv interface{}, ctx context.Context, dec 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _MemeService_GetTimelineMemes_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetTimelineRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MemeServiceServer).GetTimelineMemes(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: MemeService_GetTimelineMemes_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MemeServiceServer).GetTimelineMemes(ctx, req.(*GetTimelineRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // MemeService_ServiceDesc is the grpc.ServiceDesc for MemeService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -228,6 +262,10 @@ var MemeService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SearchMemes",
 			Handler:    _MemeService_SearchMemes_Handler,
+		},
+		{
+			MethodName: "GetTimelineMemes",
+			Handler:    _MemeService_GetTimelineMemes_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
