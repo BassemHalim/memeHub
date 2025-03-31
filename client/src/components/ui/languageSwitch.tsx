95e8d66c92237d16ac5d1c85654a7767b0a96a67
BassemHalim <bassemamir459@gmail.com>
2025-03-30 22:51:08 -0700

    feat: manually setting "NEXT_LOCALE" cookie instead of Next-intl

--- client/src/components/ui/languageSwitch.tsx
+++ client/src/components/ui/languageSwitch.tsx
@@ -1,29 +1,64 @@
-'use client'
+"use client";
 import { usePathname, useRouter } from "@/i18n/navigation";
 import { Languages } from "lucide-react";
 import { Locale, useLocale } from "next-intl";
 import { useParams, useSearchParams } from "next/navigation";
-import { useTransition } from "react";
+import { useEffect, useTransition } from "react";
 import { Button } from "./button";
 
-export default function LanguageSwitch() {
+// In this example the locale is read from a cookie. You could alternatively
+// also read it from a database, backend service, or any other source.
+const COOKIE_NAME = "NEXT_LOCALE";
 
-    const isArabic = useLocale() == 'ar'
+const setLocale = (value:Locale, days = 365) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${COOKIE_NAME}=${value};${expires};path=/`;
+};
+
+// Helper function to get cookies with vanilla JS
+const getLocale = () => {
+    const cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith(`${COOKIE_NAME}=`));
+    return cookieValue ? cookieValue.split('=')[1] : null;
+};
+
+export default function LanguageSwitch() {
+    const isArabic = useLocale() == "ar";
     const router = useRouter();
     const [, startTransition] = useTransition();
     const pathname = usePathname();
     const params = useParams();
-    const searchParams = useSearchParams()
-const query = searchParams.get('query')
-    function setUserLocale(nextLocale: Locale) {
-        startTransition(() => {
-            router.replace(
-                // @ts-expect-error -- TypeScript will validate that only known `params`
-                // are used in combination with a given `pathname`. Since the two will
-                // always match for the current route, we can skip runtime checks.
-                query? { pathname, params, query: {query: searchParams.get('query')} }: {pathname, params},
-                { locale: nextLocale }
+    const searchParams = useSearchParams();
 
+    useEffect(() => {
+        const locale = getLocale();
+        if (!locale) {
+            setLocale("ar");
+        }
+    }, [isArabic]);
+
+    function setUserLocale(nextLocale: Locale) {
+        setLocale(nextLocale);
+        startTransition(() => {
+            // Create a new URLSearchParams instance to properly handle query parameters
+            const newSearchParams = new URLSearchParams(
+                searchParams.toString()
+            );
+
+            router.replace(
+                {
+                    pathname,
+                    // @ts-expect-error -- TypeScript will validate that only known `params` are used in combination with a given `pathname`. Since the two will always match for the current route, we can skip runtime checks.
+                    params,
+                    // Only include query if there are search parameters
+                    ...(newSearchParams.toString()
+                        ? { query: Object.fromEntries(newSearchParams) }
+                        : {}),
+                },
+                { locale: nextLocale }
             );
         });
     }
